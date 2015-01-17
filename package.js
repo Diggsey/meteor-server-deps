165f5b81cb617019c94d9f2c7a1843a3598baa27
Mitar <mitar.git@tnode.com>
2015-01-17 10:11:29 -0800

    Restructured the package.

--- package.js
+++ package.js
@@ -1,11 +1,15 @@
 Package.describe({
-  summary: "Enable server-side reactivity"
+  summary: "Server-side Tracker.autorun",
+  version: '0.2.0',
+  name: 'peerlibrary:server-autorun',
+  git: 'https://github.com/peerlibrary/meteor-server-autorun.git'
 });
 
-Package.on_use(function(api) {
-  api.use('coffeescript', ['server']);
-  api.use('deps', ['server']);
-  api.use('underscore', ['server']);
+Package.on_use(function (api) {
+  api.versionsFrom('METEOR@1.0.2.1');
+  api.use(['coffeescript', 'underscore', 'tracker'], 'server');
 
-  api.add_files('lib/server-deps.coffee', ['server']);
+  api.add_files([
+    'server.coffee'
+  ], 'server');
 });
