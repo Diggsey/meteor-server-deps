bb90da6daaa46ed0d4c71607a25993197b2feb5a
Mitar <mitar.git@tnode.com>
2015-01-21 10:09:59 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Server-side Tracker.autorun",
-  version: '0.2.1',
+  version: '0.2.2',
   name: 'peerlibrary:server-autorun',
   git: 'https://github.com/peerlibrary/meteor-server-autorun.git'
 });
