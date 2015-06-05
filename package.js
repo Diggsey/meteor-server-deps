6f6a4ea9e71b9531e8bb5882e03367e0cf264188
Jernej Kos <jernej@kos.mx>
2015-06-05 15:16:50 +0200

    Bumped package version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Server-side Tracker.autorun",
-  version: '0.2.4',
+  version: '0.2.5',
   name: 'peerlibrary:server-autorun',
   git: 'https://github.com/peerlibrary/meteor-server-autorun.git'
 });
